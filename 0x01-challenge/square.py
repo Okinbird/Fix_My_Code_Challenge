b977fad7d6225ebf44b4edb5c4159df8779ed3f2
Okinbird <okinyinka@yahoo.co.uk>
2023-02-07 13:57:23 -0800

    fix code

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
+"""Defines a class Square"""
+
 
 class square():
-    
+    """Class Square"""
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
+        """initializes the square"""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -15,10 +17,13 @@ class square():
         return self.width * self.height
 
     def PermiterOfMySquare(self):
+        """ Perimeter of the sqaure """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """String representation"""
         return "{}/{}".format(self.width, self.height)
+
 
 if __name__ == "__main__":
 
